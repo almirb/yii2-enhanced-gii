9c1869c64624034b6317e27484422697d9e5bdf2
Almir Bolduan <almir.bsi@gmail.com>
2024-06-25 15:11:38 -0300

    fix: Updated Generator.php to avoid PHP 8.1 erros on trim

--- model/Generator.php
+++ model/Generator.php
@@ -102,7 +102,7 @@ class Generator extends \yii\gii\Generator {
      */
     public function rules() {
         return array_merge(parent::rules(), [
-            [['db', 'nsModel', 'viewPath', 'nsController', 'nsTraits', 'tableName', 'modelClass', 'searchModelClass', 'nsSearchModel', 'baseControllerClass', 'queryNs', 'nsController'], 'filter', 'filter' => 'trim'],
+            [['db', 'nsModel', 'viewPath', 'nsController', 'nsTraits', 'tableName', 'modelClass', 'searchModelClass', 'nsSearchModel', 'baseControllerClass', 'queryNs', 'nsController'], 'filter', 'filter' => 'trim', 'skipOnEmpty' => true],
             [['tableName', 'baseControllerClass', 'indexWidgetType', 'db'], 'required'],
             [['tableName'], 'match', 'pattern' => '/^(\w+\.)?([\w\*]+)$/', 'message' => 'Only word characters, and optionally an asterisk and/or a dot are allowed.'],
             [['tableName'], 'validateTableName'],
